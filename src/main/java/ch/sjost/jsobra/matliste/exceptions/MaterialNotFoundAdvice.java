dc31e504d51a0d94612c15bc6d2b006fae20b65a
jostsa <samuel.jost.1@post.ch>
2021-11-24 10:57:15 +0100

    Added Advice to return 404

---
+++ src/main/java/ch/sjost/jsobra/matliste/exceptions/MaterialNotFoundAdvice.java
@@ -0,0 +1,18 @@
+package ch.sjost.jsobra.matliste.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class MaterialNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(MaterialNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String employeeNotFoundHandler(MaterialNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
